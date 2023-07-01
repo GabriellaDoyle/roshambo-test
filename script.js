240fa87f5ee615ac451224f1bba8974fd3991f5e
Gabriella Doyle <Gabrielladoyle2001@gmail.com>
2023-07-01 15:21:32 +0100

    event-listener-error

---
+++ script.js
@@ -0,0 +1,21 @@
+var you;
+var yourScore = 0;
+var challenger;
+var challengersScore;
+
+var choices = ["rock","paper","scissors"];
+
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+}
